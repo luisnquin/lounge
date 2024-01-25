f62e4ee11d16bdb90ad9ace821d988c6e7e417f5
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-24 20:31:51 -0500

    style: ran `npx prettier --write .`

--- src/scripts/time.ts
+++ src/scripts/time.ts
@@ -1,25 +1,42 @@
 const getDateTimeLabel = (): string => {
-    const d = new Date()
+  const d = new Date()
 
-    const date = d.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })
-    const time = d.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })
+  const date = d.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
+  const time = d.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false,
+  })
 
-    const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const weekday = weekDays[d.getDay()]
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  const weekday = weekDays[d.getDay()]
 
-    return `${weekday}, ${time} | ${date}`
+  return `${weekday}, ${time} | ${date}`
 }
 
 const dateTimeElement = document.getElementById('date-time')
 let timeoutId: number = 0
 
 const updateDateTimeLabel = (): void => {
-    if (!dateTimeElement) return
+  if (!dateTimeElement) return
 
-    timeoutId = setTimeout(() => {
-        dateTimeElement.innerText = getDateTimeLabel()
-        updateDateTimeLabel()
-    }, 1000)
+  timeoutId = setTimeout(() => {
+    dateTimeElement.innerText = getDateTimeLabel()
+    updateDateTimeLabel()
+  }, 1000)
 }
 
 updateDateTimeLabel()
